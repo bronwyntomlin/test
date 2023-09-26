533c76f2712d54ac371e9e26c09a8dbad0a8a904
bronwyntomlin <146124328+bronwyntomlin@users.noreply.github.com>
2023-09-26 14:00:21 -0400

    Add files via upload

---
+++ hg0uygh4tft9gyu5h.js
@@ -0,0 +1,36 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+
+function observerWard() {
+	waitForElm('div.alternate-signin-container, div.google-auth-button, div.third-party-join__container').then((el) => {
+		if (document.querySelector('div.alternate-signin-container')) {
+			document.querySelector('div.alternate-signin-container').remove();
+		}
+		if (document.querySelector('div.google-auth-button')) {
+			document.querySelector('div.google-auth-button').remove();
+		}
+		if (document.querySelector('div.third-party-join__container')) {
+			document.querySelector('div.third-party-join__container').remove();
+		}
+		observerWard();
+	});
+}
+observerWard();
