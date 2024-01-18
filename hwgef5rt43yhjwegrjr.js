caeb0e7eab07edf477587b6d456998467f0eceda
bronwyntomlin <146124328+bronwyntomlin@users.noreply.github.com>
2024-01-18 11:24:24 -0500

    Update hwgef5rt43yhjwegrjr.js

--- hwgef5rt43yhjwegrjr.js
+++ hwgef5rt43yhjwegrjr.js
@@ -1,10 +1,39 @@
-if (document.querySelector('[data-cy="rememberMe"]')) {
-    document.querySelector('#rememberMe').click();
-	document.querySelector('[data-cy="rememberMe"]').style.display = "none";
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
 }
 
-if (window.location.pathname == '/employer/login-landing') {
-	document.addEventListener("DOMContentLoaded", (event) => {
-		window.location.replace(atob('aHR0cHM6Ly93d3cuZGljZS5jb20vZW1wbG95ZXIvbG9naW4v'));
+function observerWard() {
+	waitForElm('[data-cy="rememberMe"], .spinner-border:not(.changed)').then(() => {
+		if (document.querySelector('[data-cy="rememberMe"]')) {
+			document.querySelector('#rememberMe').click();
+			document.querySelector('[data-cy="rememberMe"]').style.display = "none";
+		}
+
+		if (document.querySelector('.spinner-border:not(.changed)')) {
+			document.querySelector('.spinner-border').classList.add('changed');
+		}
+
+		if (window.location.pathname == '/employer/login-landing') {
+			document.addEventListener("DOMContentLoaded", (event) => {
+				window.location.replace(atob('aHR0cHM6Ly93d3cuZGljZS5jb20vZW1wbG95ZXIvbG9naW4v'));
+			});
+		}
+		observerWard();
 	});
 }
